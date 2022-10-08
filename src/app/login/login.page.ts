bfcb773e4d689a1b4c0d00828c002219e3f77955
robby <robby.joker@gmail.com>
2022-10-08 14:13:01 +0200

    esercizi con array

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Persona } from '../models/Persona';
 
 @Component({
   selector: 'app-login',
@@ -11,14 +12,39 @@ export class LoginPage implements OnInit {
 
   constructor(private router: Router){}
 
+  arrayPersone: Persona[];
+
   varNome: string = "";
   varPsw: string = "";
   varBool: boolean;
   showPsw: boolean = true;
   varType: string = "text";
   varIcon: string = "eye-off-outline";
+  myArray: string[] = ['prim_Elem', 'second_Elem', 'terzo_EL'];
+
 
   ngOnInit() {
+    console.log(this.myArray);
+
+    for (let i = 0; i < this.myArray.length; i++) { /* Primo tipo di FOR */
+      console.log(this.myArray[i], 'primo metodo FOR');
+    }
+
+    for (let valore of this.myArray){ /* Secondo tipo di FOR */
+      console.log(valore, 'secondo metodo FOR');
+    }
+
+    let iterabile =this.myArray.values() /* Terzo tipo di FOR */
+    for (let i of iterabile){
+      console.log(i, 'terzo metodo FOR');
+    }
+
+    this.myArray.forEach(
+      elemento =>{
+        console.log(elemento, 'quarto metodo FOR');
+      }
+    )
+    
   }
 
   logMe(){
---
+++ src/app/models/Persona.ts
@@ -0,0 +1,5 @@
+export interface Persona {
+    name: string;
+    surname: string;
+    age: number;
+}
